d8763c8ff4154634b2ce78f2b3784c3c232decd6
savan <savan@10.10.6.75>
2019-02-07 19:03:41 +0530

    Code checked in

--- Ecommerce/src/Testscript1.java
+++ Ecommerce/src/Testscript1.java
@@ -1,13 +1,15 @@
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 
@@ -16,7 +18,11 @@ public class Testscript1 {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		File file = new File("C:/Users/Savan/git/Ecommerce/Library/chromedriver.exe");
+		try {
+		
+	
+		
+		File file = new File("C:/Users/savan/git/Ecommerce/Ecommerce/Library/chromedriver.exe");
 		System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
  		
 		ChromeDriver driver =  new ChromeDriver();
@@ -25,28 +31,59 @@ public class Testscript1 {
  		    
  		    driver.manage().window().maximize();
  		    
+ 		    
  		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+ 		   
  		    
  		     driver.findElement(By.name("keyword")).sendKeys("TSHIRT");
  		     
- 		    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            
- 		     driver.findElement(By.className("searchTextSpan")).click();
  		     
  		    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
  		    
- 		    driver.findElement(By.xpath("//*[@id='content_wrapper']/div[7]/div[4]/div/div[1]/div[2]/div[3]/div[3]/div/div[1]")).click();
+            
+ 		     driver.findElement(By.className("searchTextSpan")).click();
+ 		     
+ 		     
+ 		    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
  		    
- 		/*List<WebElement> options  =  driver.findElements(By.xpath("//div[7]/div[4]/div/div[1]/div[2]/div[3]"));
+ 		    
+ 		   driver.findElement(By.xpath("//*[@id='content_wrapper']/div[7]/div[4]/div/div[1]/div[2]/div[3]/div[3]/div/div[1]")).click();
  		
- 		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- 		    
- 		 for(WebElement option : options)
- 		    {
- 		    	 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- 		    	 
- 		    	   System.out.println(option.getAttribute("value"));
- 		    }*/
-	}
-
+ 		
+ 		   Thread.sleep(5000);
+ 		   
+ 		  	    
+ 		 driver.findElement(By.xpath("/html/body/div[11]/div[7]/div[4]/div/div[1]/div[2]/div[4]/div[3]/div/div[1]")).click();
+ 		 
+ 	//	String windowHandle = driver.getWindowHandle();
+ 		 
+ 		  Thread.sleep(5000);
+ 		 
+ 		 driver.findElement(By.linkText("product-title")).click();
+ 		  		
+ 		
+ 	/*	Thread.sleep(5000);
+ 				
+ 		Actions action= new Actions(driver);
+ 		
+ 		action.keyDown(Keys.CONTROL).sendKeys(Keys.TAB).build().perform();
+ 		
+ 		driver.findElement(By.xpath("/html/body/div[11]/div[7]/div[5]/div[3]/div[7]/section/div/div[3]/div[1]/a")).click();
+ 		 		
+ 				 		
+ 		Thread.sleep(5000);
+ 		
+ 		
+ 		driver.findElement(By.name("inputValEnter")).sendKeys("test");*/
+ 		
+		}
+		
+		catch (Exception e) {
+			
+			System.out.println(e);
+		}
+ 		 
+ 		
+ 		
+}
 }
